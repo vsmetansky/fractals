a2494010496a3bb039823518c0fa592813315986
vladsmetansky <vladsmetansky@gmail.com>
2020-02-11 00:15:11 +0200

    minor changes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,31 @@
 <script>
-	export let name;
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    font-size: 4em;
+    font-weight: 100;
+    animation: 1.5s intro;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @keyframes intro {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 </style>
+
+<main>
+  <h1>Fractals</h1>
+</main>
